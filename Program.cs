bd2305ebd160bbecdcf30f56f3391d3f09ed3157
medvedaleksey <60468619+medvedaleksey@users.noreply.github.com>
2020-10-24 12:56:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ProgressionLibrary;
+
+namespace ConsoleProgression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Progression progr1 = new Progression();
+            Console.WriteLine("Введите первый элемент и коэффициент прогрессии");
+            progr1.Input();
+            Progression progr2 = new Progression(1, 2);
+            Console.WriteLine("Сумма прогрессий " + (progr1 + progr2));
+            Console.WriteLine("Разность первой и второй прогрессии " + (progr1 - progr2));
+            Console.WriteLine("Седьмой элемент второй прогрессии " + progr2[7]);
+            if (progr1)
+                Console.WriteLine("Прогрессия 1 возрастающая");
+            else
+                Console.WriteLine("Прогрессия 1 убывающая");
+            Console.WriteLine("Прогрессии равны " + (progr1 == progr2));
+            Console.WriteLine("Первый элемент прогрессии 2 " + (double)progr2);
+            Console.WriteLine("Создадим 3 прогрессию с разностью 1 и первым элементом:");
+            double d = Convert.ToDouble(Console.ReadLine());
+            Progression progr3 = new Progression();
+            progr3 = d;
+            Console.WriteLine(progr3);
+            Console.WriteLine("Умножим 3 прогрессию на 5 " + progr3 * 5);
+        }
+    }
+}
---
+++ Progression.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace ProgressionLibrary
+{
+    public class Progression
+    {
+        private double a1;
+        private double d;
+
+       public double FirstElem
+            {
+            get
+                {
+                    return a1;
+                }
+                set
+                {
+                    a1 = value;
+                }
+            }
+    public double Diff
+    {
+        get
+        {
+            return d;
+        }
+        set
+        {
+            d = value;
+        }
+    }
+        public void Input()
+        {
+            double first = Convert.ToDouble(Console.ReadLine());
+            double diff = Convert.ToDouble(Console.ReadLine());
+
+            Diff = diff; 
+            FirstElem = first;
+        }
+        public Progression(double first, double diff)
+        {
+            FirstElem = first;
+            Diff = diff;
+        }
+
+        public Progression(Progression progr)
+        {
+            FirstElem = progr.FirstElem;
+            Diff = progr.Diff;
+        }
+        public Progression() : this(0, 0)
+        {
+        }
+        public static Progression operator *(Progression progr1, double d)
+        {
+            Progression progr = new Progression();
+            progr.FirstElem = progr1.FirstElem * d;
+            progr.Diff = progr1.Diff * d;
+            return progr;
+        }
+        public static Progression operator +(Progression progr1, Progression progr2)
+        {
+            Progression progr = new Progression();
+            progr.FirstElem = progr1.FirstElem + progr2.FirstElem;
+            progr.Diff = progr1.Diff + progr2.Diff;
+            return progr;
+        }
+        public static Progression operator -(Progression progr1, Progression progr2)
+        {
+            Progression progr = new Progression();
+            progr.FirstElem = progr1.FirstElem - progr2.FirstElem;
+            progr.Diff = progr1.Diff - progr2.Diff;
+            return progr;
+        }
+        public override string ToString()
+        {
+            return "Начальный элемент " + FirstElem + " Коэффициент " + Diff;
+        }
+        public static bool operator ==(Progression progr1, Progression progr2)
+        {
+            if ((progr1.Diff == progr2.Diff) && (progr1.FirstElem == progr2.FirstElem))
+                return true;
+            return false;
+        }
+
+        public static bool operator !=(Progression progr1, Progression progr2)
+        {
+            if ((progr1.Diff != progr2.Diff) || (progr1.FirstElem != progr2.FirstElem))
+                return true;
+            return false;
+        }
+        public static bool operator true(Progression progr)//определение является ли прогрессия возрастающей
+        {
+            return progr.Diff > 0;
+        }
+        public static bool operator false(Progression progr)//определение является ли прогрессия убывающей
+        {
+            return progr.Diff < 0;
+        }
+        public double this[int n]
+        {
+            get { return FirstElem + (n-1)*Diff; }
+        }
+        public static explicit operator double(Progression progr)//явное приведение типов
+        {
+            return progr.FirstElem;
+        }
+
+        public static implicit operator Progression(double first)//неявное приведение типов
+        {
+            Progression progr = new Progression();
+            progr.FirstElem = first;
+            progr.Diff = 1;
+            return progr;
+        }
+    }
+}
